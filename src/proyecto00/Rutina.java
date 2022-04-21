79d934a0cbd547a6a4f3dea3321f2ee8b3a6264a
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-21 13:15:29 -0600

    test6

--- src/proyecto00/Rutina.java
+++ src/proyecto00/Rutina.java
@@ -4,17 +4,24 @@ public class Rutina {
 private Persona usuario []=new Persona [1];    
     
 public void predefinirUsuario (){
-  usuario[0]=new Persona ("UsuarioPorDefecto","correo@email.com",555555);     
+  usuario[0]=new Persona ("Sin Usuario","Sin correo electronico",00000000);     
 }//fin de ingresarDatosUsuario
 
-public void motrarUsuario(){
-    int x=0;
-  JOptionPane.showMessageDialog(null," Nombre: " + usuario[x].getNombre()
-                                +"\n Email: " + usuario[x].getEmail()
-                                +"\n Numero de Telefono: " + usuario[x].getNumeroTelefono());     
+public void mostrarUsuario(){
+    //int x=0;
+  JOptionPane.showMessageDialog(null," Nombre: " + usuario[0].getNombre()
+                                +"\n Email: " + usuario[0].getEmail()
+                                +"\n Numero de Telefono: " + usuario[0].getNumeroTelefono());     
 }
-  
-    
+
+public void modificarUsuario (){
+    Persona p=new Persona(); 
+    p.setNombre( JOptionPane.showInputDialog(null,"Modificar el nombre de Usuario:"));
+    p.setEmail(JOptionPane.showInputDialog(null,"Modificar el email del Usuario:"));
+    p.setNumeroTelefono(Integer.parseInt(JOptionPane.showInputDialog(null,"Modificar el numero de telefono del Usuario:")));
+    usuario[0]=p;
+} //fin de modificarUsuario
+     
 public void ingresarDatosPropiedadConstruida () {
     
 } // fin de   ingresarDatosPropiedadConstruida
