6076c67d7ef7b17f3f096fab8d7e19be4b3fe49b
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-20 22:56:01 -0600

    test5

--- src/proyecto00/Main.java
+++ src/proyecto00/Main.java
@@ -2,11 +2,14 @@ package proyecto00;
 import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        provincias p=new provincias();
-        Descuento3 d=new Descuento3();
-        p.select();
-        d.referirNombres();
-        d.mostrarNombresReferidos();
+        Menu1 m=new Menu1();
+        Provincias p=new Provincias();
+        Descuento d=new Descuento();  
+        m.responderMenuPrincipal();
+        //m.responderMenuCompraVenta(); borrar
+        //p.selecionarProvincia();
+        /*d.referirNombres();
+        d.mostrarNombresReferidos();*/
         
     }    
 }
