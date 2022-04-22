acf83b7d5ca82adbd465cce04fa9ee51db9872d5
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-22 11:32:34 -0600

    test7

--- src/proyecto00/Rutina.java
+++ src/proyecto00/Rutina.java
@@ -1,8 +1,11 @@
 package proyecto00;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import javax.swing. JOptionPane;
 public class Rutina {
 private Persona usuario []=new Persona [1];    
-    
+     
 public void predefinirUsuario (){
   usuario[0]=new Persona ("Sin Usuario","Sin correo electronico",00000000);     
 }//fin de ingresarDatosUsuario
@@ -11,7 +14,7 @@ public void mostrarUsuario(){
     //int x=0;
   JOptionPane.showMessageDialog(null," Nombre: " + usuario[0].getNombre()
                                 +"\n Email: " + usuario[0].getEmail()
-                                +"\n Numero de Telefono: " + usuario[0].getNumeroTelefono());     
+                                +"\n Numero de Telefono: " + usuario[0].getNumeroTelefono());  
 }
 
 public void modificarUsuario (){
@@ -25,6 +28,27 @@ public void modificarUsuario (){
 public void ingresarDatosPropiedadConstruida () {
     
 } // fin de   ingresarDatosPropiedadConstruida
+
+
+public void mostrarFinal(){
+        try {
+            String ruta = "C:\\Users\\janjo\\OneDrive\\Documentos\\Universidad\\Progra\\projects\\proyecto00\\usuario.txt";
+            String contenido = (" Nombre: " + usuario[0].getNombre()
+                                +"\n Email: " + usuario[0].getEmail()
+                                +"\n Numero de Telefono: " + usuario[0].getNumeroTelefono());
+            File file = new File(ruta);
+            // Si el archivo no existe es creado
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(contenido);
+            bw.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 } // fin de rutina 
