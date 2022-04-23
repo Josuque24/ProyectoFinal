def8172b9625cd4818c2da657c2be8c93d8da1c7
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-22 22:40:44 -0600

    test9

--- src/proyecto00/Menu2.java
+++ src/proyecto00/Menu2.java
@@ -1,7 +1,11 @@
 package proyecto00;
 import javax.swing.JOptionPane;
 public class Menu2 {
-private byte respuestaMenuCompraVenta;    
+    private byte respuestaMenuCompraVenta; 
+    private byte respuestaMenuCompra;
+    private byte respuestaMenuVenta;
+    
+    
     public void responderMenuCompraVenta(){
         while(respuestaMenuCompraVenta!=3){
         respuestaMenuCompraVenta=Byte.parseByte(JOptionPane.showInputDialog (null,
@@ -9,8 +13,51 @@ private byte respuestaMenuCompraVenta;
                                             +"1) Vender \n"
                                             +"2) Comprar \n" 
                                             +"3) Salir"
-                                            ));             
-            switch(respuestaMenuCompraVenta){
+                                            )); 
+            if(respuestaMenuCompraVenta==1){
+                
+                Venta v=new Venta ();
+                while(respuestaMenuVenta !=3){
+                    respuestaMenuVenta = Byte.parseByte(JOptionPane.showInputDialog (null,""
+                        + "¿Que desea?" 
+                        + "1) Vender un lote baldío."
+                        + "2) Vender un lote con un edificio ya construido."
+                        + "3) Salir de este menu."    ));
+                    if(respuestaMenuVenta==1){
+                        v.ingresarDatosPropiedadBaldia();
+                    }if(respuestaMenuVenta==2){
+                        v.ingresarDatosPropiedadConstruida();
+                    }else{
+                        JOptionPane.showMessageDialog(null,"Escribio un numero que no es parte del menu.");
+                    }//fin de if
+                } // fin de while
+                
+            }if(respuestaMenuCompraVenta==2){
+                
+                while(respuestaMenuCompra !=3){
+                    respuestaMenuCompra = Byte.parseByte(JOptionPane.showInputDialog (null,""
+                        + "¿Que desea?" 
+                        + "1) Comprar un lote baldío."
+                        + "2) Comprar un lote con un edificio ya construido."
+                        + "3) Salir de este menu."    ));
+                    if(respuestaMenuCompra==1){
+
+                    }if(respuestaMenuCompra==2){
+
+                    }else{
+                        JOptionPane.showMessageDialog(null,"Escribio un numero que no es parte del menu.");
+                    }//fin de if                 
+                } // fin de while
+                
+            }else{
+                JOptionPane.showMessageDialog(null,"Escribio un numero que no es parte del menu.");
+            }
+        } //fin de while   
+    }//fin menuCompraVenta   
+} //fin responderMenuCompraVenta
+
+
+/* switch(respuestaMenuCompraVenta){
                   case 1:{
 
                     break; 
@@ -26,7 +73,4 @@ private byte respuestaMenuCompraVenta;
                   default:{
                      JOptionPane.showMessageDialog(null,"Escribio un numero que no es parte del menu.");
                   }
-            } //fin de switch
-        } //fin de while   
-    }//fin menuCompraVenta
-} //fin responderMenuCompraVenta
+            } //fin de switch */
