c6ffc85931075d40a1d9a45b7e45d1ff5f086d9a
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-23 02:49:40 -0600

    test28

--- src/proyecto00/Main.java
+++ src/proyecto00/Main.java
@@ -2,13 +2,10 @@ package proyecto00;
 import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        Menu1 m=new Menu1();
-        Provincias p=new Provincias();
-        Descuento d=new Descuento();  
-        m.responderMenuPrincipal();
-        //p.selecionarProvincia();
-        //d.referirNombres();
-        //d.mostrarNombresReferidos();
-        
+        Menu1 m=new Menu1(); 
+        JOptionPane.showMessageDialog(null,"El programa empieza con un usuario por defecto. \n"
+            + "Por favor modifique la informacion de usuario con su informacion en el siguiente menu."
+            , "Advertencia", JOptionPane.INFORMATION_MESSAGE);
+        m.responderMenuPrincipal();                
     }  
 }
