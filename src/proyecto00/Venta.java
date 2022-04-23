e66a43c7665e310c7e3e8ec94fd9c95e9b69bee2
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-23 00:15:15 -0600

    test11

--- src/proyecto00/Venta.java
+++ src/proyecto00/Venta.java
@@ -18,24 +18,53 @@ public class Venta {
                 "Ingrese la cantidad de baños que hay en la propiedad:")));
         pro.setComedor(Byte.parseByte(JOptionPane.showInputDialog(null,
                 "Cuenta la propiedad con un comedor?\"\n" +
-                "+\"0)No 1) Si ")));
+                "0)No \n"+ "1) Si ")));
         pro.setSala(Byte.parseByte(JOptionPane.showInputDialog(null,
                 "Cuenta la propiedad con una sala?\"\n" +
-                "+\"0)No 1) Si ")));
+                "0)No \n"+ "1) Si ")));
         pro.setGaraje(Byte.parseByte(JOptionPane.showInputDialog(null,
                 "Cuenta la propiedad con un garaje?\"\n" +
-                "+\"0)No 1) Si ")));
+                "0)No \n"+ "1) Si ")));
         pro.setCuartoLavado(Byte.parseByte(JOptionPane.showInputDialog(null,
-                "")));
+                "Cuenta la propiedad con un cuarto de lavado? \n"+
+                "0)No \n"+ "1) Si ")));
         venderPropiedad[0]=pro;        
     } // fin de   ingresarDatosPropiedadConstruida
     
+    
+    public void mostrarDatosPropiedadConstruida () {
+       String a,b,c,d,e,f;
+       if(venderPropiedad[0].getMedidasPatio()>0){
+        a="La propiedad tiene patio de "+venderPropiedad[0].getMedidasPatio()+ " m²" ;
+       }else{
+        a="La propiedad no tiene patio.";   
+       }
+       if(venderPropiedad[0].getComedor()==1){b= "Si";}else{b= "No";}
+       if(venderPropiedad[0].getSala()==1){c="Si";}else{c="No";}
+       if(venderPropiedad[0].getGaraje()==1){d="Si";}else{d="No";}
+       if(venderPropiedad[0].getCuartoLavado()==1){e="Si";}else{e="No";}
+       if(venderPropiedad[0].getCantBaños()>1){f=" baño";}else{f=" baños";}
+       
+       String s="El valor de la propiedad es: "+ venderPropiedad[0].getValorPropiedad()+"\n"
+               +"La propiedad tiene "+venderPropiedad[0].getMedidasPropiedad() + " m² \n" 
+               +"La propiedad tiene "+venderPropiedad[0].getCantHabitaciones()+" habitaciones. \n"
+               + a + "\n"
+               +"La propiedad tiene "+venderPropiedad[0].getCantBaños()+f+" \n"
+               +"la propiedad tiene comedor: "+ b +"\n"
+               +"La propiedad tiene sala: "+ c + "\n"
+               +"La propiedad tiene garaje: "+ d +"\n"
+               +"La cantidad tiene cuarto de lavado: "+ e +"\n";
+       JOptionPane.showMessageDialog(null,s);
+   } 
+    
     public void ingresarDatosPropiedadBaldia (){
+        String s=" ";
         Propiedad pro=new Propiedad ();
         pro.setValorPropiedad(Integer.parseInt(JOptionPane.showInputDialog(null,
                 "Ingresar el valor de la propiedad:")));
         pro.setMedidasPropiedad(Integer.parseInt(JOptionPane.showInputDialog(null,
                 "Ingresar la cantidad de metros cuadrados que tiene la propiedad:")));
+        venderPropiedad[0]=pro; 
     } // fin de ingresarDatosPropiedadBaldia
     
 } // fin de compra
