ed9487b68a91edb114856cfbc28a54535db3cfa2
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-23 02:47:59 -0600

    test27

--- src/proyecto00/Menu1.java
+++ src/proyecto00/Menu1.java
@@ -10,14 +10,12 @@ private byte respuestaMenuPrincipal;
     JOptionPane.showMessageDialog(null,"El programa empieza con un usuario por defecto. \n"
             + "Por favor modifique la informacion de usuario con su informacion en el siguiente menu."
             , "Advertencia", JOptionPane.INFORMATION_MESSAGE);
-        while(respuestaMenuPrincipal!=6){
+        while(respuestaMenuPrincipal!=4){
         respuestaMenuPrincipal= Byte.parseByte(JOptionPane.showInputDialog (null,"Menu pricipal \n"
                 +"1) Mostrar los datos de usuario almacenados. \n"
                 +"2) Modifique la informacion de usuario. \n"
-                +"3) Entrar en menu de compra y venta. \n"
-                +"4) Aplicar por un descuento \n"
-                +"5) Guardar la informacion de usuario para referirla a los colaboradores. \n"
-                +"6) salir \n" ,"Menu",JOptionPane.INFORMATION_MESSAGE ));
+                +"3) Entrar en menu de compra y venta. \n"                                
+                +"4) salir \n" ,"Menu",JOptionPane.INFORMATION_MESSAGE ));
             switch(respuestaMenuPrincipal){
                   case 1:{
                      r.mostrarUsuario();
@@ -25,6 +23,11 @@ private byte respuestaMenuPrincipal;
                   }
                   case 2:{
                     r.modificarUsuario();
+                    r.archivarUsuario();
+                    JOptionPane.showMessageDialog(null,"Se guardo la informacion de usuario,"+
+                            " esta se enviara a nuestros colaboradores para que ellos se pongan en contacto con usted. \n"
+                            + "La informacion que usted disponga mas adelante tambien sera referida a ellos."
+                            );
                      break;
                   }
                   case 3:{
@@ -34,16 +37,7 @@ private byte respuestaMenuPrincipal;
                    case 4:{
                     
                      break;
-                  }
-                   case 5:{
-                    r.archivarUsuario();
-                    JOptionPane.showMessageDialog(null,"Ya se guardo la informacion de usuario.");
-                     break;
-                  }
-                   case 6:{
-                       
-                     break;  
-                  }
+                  }                   
                   default:{
                      JOptionPane.showMessageDialog(null,"Escribio un numero que no es parte del menu.");
                   }
