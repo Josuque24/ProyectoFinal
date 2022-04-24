e36595c603e5cdb9601e6caa3149219527dcdb8f
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-24 00:02:57 -0600

    test36

--- src/proyecto00/Main.java
+++ src/proyecto00/Main.java
@@ -6,7 +6,7 @@ public class Main {
         JOptionPane.showMessageDialog(null,"A continuacion el programa la pedira que ingrese su informacion personal. \n                       "
             + "Despues de eso podra ingresar al menu principal.\n"
             + "La informacion que provea aqui sera referida a un asesor de bienes raises. \n"
-            + "El acesor se pondra en contacto con usted enlas proximas 48 horas"    
+            + "El acesor se pondra en contacto con usted en las proximas 48 horas."    
             , "Advertencia", JOptionPane.INFORMATION_MESSAGE);
         m.responderMenuPrincipal();                
     }  
