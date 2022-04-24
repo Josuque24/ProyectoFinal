62506a1d01b21bcb853742ef9d5100e72400a131
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-23 23:55:23 -0600

    test35

--- src/proyecto00/Main.java
+++ src/proyecto00/Main.java
@@ -3,8 +3,10 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
         Menu1 m=new Menu1(); 
-        JOptionPane.showMessageDialog(null,"El programa empieza con un usuario por defecto. \n"
-            + "Por favor modifique la informacion de usuario con su informacion en el siguiente menu."
+        JOptionPane.showMessageDialog(null,"A continuacion el programa la pedira que ingrese su informacion personal. \n                       "
+            + "Despues de eso podra ingresar al menu principal.\n"
+            + "La informacion que provea aqui sera referida a un asesor de bienes raises. \n"
+            + "El acesor se pondra en contacto con usted enlas proximas 48 horas"    
             , "Advertencia", JOptionPane.INFORMATION_MESSAGE);
         m.responderMenuPrincipal();                
     }  
