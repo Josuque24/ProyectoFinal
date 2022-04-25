7eddd3968216bbed0197281ccda9b6b5ee25c105
Josuque24 <102047929+Josuque24@users.noreply.github.com>
2022-04-24 23:42:42 -0600

    test44

--- src/proyecto00/Main.java
+++ src/proyecto00/Main.java
@@ -2,12 +2,7 @@ package proyecto00;
 import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
-        Menu1 m=new Menu1(); 
-        JOptionPane.showMessageDialog(null,"A continuacion el programa la pedira que ingrese su informacion personal. \n                       "
-            + "Despues de eso podra ingresar al menu principal.\n"
-            + "La informacion que provea aqui sera referida a un asesor de bienes raises. \n"
-            + "El acesor se pondra en contacto con usted en las proximas 48 horas."    
-            , "Advertencia", JOptionPane.INFORMATION_MESSAGE);
+        Menu1 m=new Menu1();        
         m.responderMenuPrincipal();                
     }  
 }
